0e88890096049aa935678a30c3726887b4591c82
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2022-04-08 23:47:12 +0100

    style the home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
-
-
+import { Link } from "react-router-dom"
 
 const HomeScreen = () => {
 
     return (
-        <div>
-
-            <h1 className="text-3xl font-bold underline">
-                Welcome to the Kanban board
-            </h1>
-        </div>
+        <main className="home-wrapper">
+            <div className="container flex column full-screen text-center">
+                <h1 className="herotext">Hey Buddy! 👋</h1>
+                <p className="sub-herotext">
+                    Welcome to the Project Task board
+                </p>
+                <Link to='/tasks'>
+                    <button className="btn">Get Started</button>
+                </Link>
+            </div>
+        </main>
     )
 }
 
