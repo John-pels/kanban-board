885c69fdbb3a322ffdb2a9c6aa9f070f0966daee
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2022-04-09 15:54:07 +0100

    Remove unused variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { CustomButton } from "../components/Common"
-import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const HomeScreen = () => {
-    const [showModal, setShowModal] = useState(false)
 
     return (
 
